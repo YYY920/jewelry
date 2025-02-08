7601d4ee41f525f1eb84bca77d578c98dd23be05
YYY920 <1715200292@qq.com>
2025-02-08 15:37:19 +1100

    update slogan and name

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,6 +1,6 @@
-export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || 'RosJewel'
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || 'Rosendal Jewellery'
 export const APP_SLOGAN =
-  process.env.NEXT_PUBLIC_APP_SLOGAN || 'Spend less, enjoy more.'
+  process.env.NEXT_PUBLIC_APP_SLOGAN || 'Unveil Timeless Elegance.'
 export const APP_DESCRIPTION =
   process.env.NEXT_PUBLIC_APP_DESCRIPTION ||
   'An jewellery store built with Next.js and MongoDB'
